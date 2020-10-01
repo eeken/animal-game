bd2d80b24b09ebb1d7463782b23c5f8a5dac19f2
Ola Wiebe <eeken07@gmail.com>
2020-10-01 12:05:32 +0200

    create farm class and logic

---
+++ Farm.js
@@ -0,0 +1,14 @@
+module.exports = class Farm {
+  
+  
+
+  constructor(address, size, owner) {
+    this.address = address;
+    this.size = size;
+    this.owner = owner;
+    this.animals = []
+  }
+
+  
+  
+};
