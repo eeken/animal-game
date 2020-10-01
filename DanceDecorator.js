3c8017a59be0ac8196f3368f7aae6ad8497d322c
Ola Wiebe <eeken07@gmail.com>
2020-10-01 12:08:33 +0200

    add danceDecorator class + logic

---
+++ DanceDecorator.js
@@ -0,0 +1,21 @@
+module.exports = class DanceDecorator {
+  
+  constructor() {
+    throw new Error("Static class!");
+  }
+
+  static teachHowToDance(animal) {
+    // Adding the dance method to the animal
+    animal.dance = this.dance;
+  }
+
+  static dance() {
+    return (
+      "I am the " +
+      this.constructor.name.toLowerCase() +
+      " " +
+      this.name +
+      " and I am dancing!"
+    );
+  }
+};
